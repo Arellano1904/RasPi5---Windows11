d6ab2473ad14f8c8ac9bd68e212ea2bd5084f30d
Arellano1904 <arellano1904@hotmail.com>
2025-07-10 11:37:34 -0600

    First commit: Added Docker and bash files related to create the RasPi5 environment.

---
+++ init-RasPi5Container.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Stop the script if an error ocurrs.
+set -e
+
+# Messages to show in container log.
+echo "***  Starting Container  ***"
+
+echo "***  Container started   ****"
+
+# Keeps the container running.
+tail -f /dev/null
